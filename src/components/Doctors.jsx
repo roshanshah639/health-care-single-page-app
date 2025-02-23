0ee40506af263e5665db30fc8fa847496e7675a1
Roshan Shah <roshanshah639@gmail.com>
2025-02-23 17:11:44 +0545

    add changes to image url

--- src/components/Doctors.jsx
+++ src/components/Doctors.jsx
@@ -7,32 +7,32 @@ import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 const Doctors = () => {
   const data = [
     {
-      img: "/src/assets/img/doc1.jpg",
+      img: "https://raw.githubusercontent.com/roshanshah639/health-care-single-page-app/refs/heads/main/src/assets/img/doc1.jpg",
       name: "Dr. Serena Mitchell",
       specialties: "Orthopedic Surgeon",
     },
     {
-      img: "/src/assets/img/doc2.jpg",
+      img: "https://raw.githubusercontent.com/roshanshah639/health-care-single-page-app/refs/heads/main/src/assets/img/doc2.jpg",
       name: "Dr. Julian Bennett",
       specialties: "Cardiologist",
     },
     {
-      img: "/src/assets/img/doc3.jpg",
+      img: "https://raw.githubusercontent.com/roshanshah639/health-care-single-page-app/refs/heads/main/src/assets/img/doc3.jpg",
       name: "Dr. Camila Rodriguez",
       specialties: "Pediatrician",
     },
     {
-      img: "/src/assets/img/doc4.jpg",
+      img: "https://raw.githubusercontent.com/roshanshah639/health-care-single-page-app/refs/heads/main/src/assets/img/doc4.jpg",
       name: "Dr. Victor Nguyen",
       specialties: "Neurologist",
     },
     {
-      img: "/src/assets/img/doc5.jpg",
+      img: "https://raw.githubusercontent.com/roshanshah639/health-care-single-page-app/refs/heads/main/src/assets/img/doc5.jpg",
       name: "Dr. Ethan Carter",
       specialties: "Dermatologist",
     },
     {
-      img: "/src/assets/img/doc6.jpg",
+      img: "https://raw.githubusercontent.com/roshanshah639/health-care-single-page-app/refs/heads/main/src/assets/img/doc6.jpg",
       name: "Dr. Olivia Martinez",
       specialties: "Ophthalmologist",
     },
